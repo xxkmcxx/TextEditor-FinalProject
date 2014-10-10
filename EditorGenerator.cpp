fef80fff73a76291d719381d7b9d261e2bcc3648
xxkmcxx <kmartinez0890@gmail.com>
2014-10-10 02:40:08 -0400

    Test
    Just testing some code  from the Book!!!

--- EditorGenerator.cpp
+++ EditorGenerator.cpp
@@ -1,10 +1,35 @@
-#include <iostream>
+// This program demonstrates the put() member function.
+ #include <iostream>
+ #include <fstream>
+ using namespace std;
 
-using namespace std;
-
-int main()
-{
-	cout << "Bitch";
-	return (0);
-
-}
+ int main()
+ {
+	 // Variable needed to open and write file.
+		 char ch;
+	
+		 // Open the file.
+		 fstream dataFile("sentence.txt", ios::out);
+	 if (!dataFile)
+		 {
+		 cout << "Error opening the file.";
+		 return 0;
+		 }
+	
+		 // Instructions to user.
+		 cout << "Type a sentence and be sure to end it with a ";
+	 cout << "period.\n";
+	
+		 // Read characters from cin and put to file.
+		 cin.get(ch);
+	 while (ch != '.')
+		 {
+		 dataFile.put(ch);
+		 cin.get(ch);
+		 }
+	 dataFile.put(ch);
+	
+		 // Close the file.
+		 dataFile.close();
+	 return 0;
+	 }
